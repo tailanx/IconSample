e252ad2eaba44acfbb10ab0b1215060b8419787b
liuyong <397021707@qq.com>
2018-02-11 10:36:58 +0800

    add

---
+++ src/assets/fonts/iconfont.js
@@ -0,0 +1,12 @@
+
+export default {
+  "fenxiang":"\ue624",
+  "guanbi":"\ue625",
+  "bofang":"\ue626",
+  "kefu":"\ue627",
+  "shenfenzheng":"\ue628",
+  "quanping":"\ue629",
+  "duigou":"\ue62a",
+  "shuoming":"\ue62b",
+  "x":"x",
+};
